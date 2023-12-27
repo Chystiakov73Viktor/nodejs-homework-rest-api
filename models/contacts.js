c9cd01838e88ad06628a6fbf9c4242ea47198d39
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-12-27 12:24:53 +0200

    added functions for data processing

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,60 @@
-// const fs = require('fs/promises')
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const { nanoid } = require("nanoid");
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, "contacts.json");
 
-const getContactById = async (contactId) => {}
+async function readFile() {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(data);
+}
 
-const removeContact = async (contactId) => {}
+async function writeFile(contacts) {
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), "utf-8");
+}
 
-const addContact = async (body) => {}
+function listContacts() {
+  return readFile();
+}
 
-const updateContact = async (contactId, body) => {}
+async function getContactById(contactId) {
+  const contacts = await readFile();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await readFile();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await writeFile(contacts);
+  return result;
+}
+
+async function addContact(body) {
+  const contacts = await readFile();
+  const newContact = {
+    id: nanoid(),
+    ...body,
+  };
+  contacts.push(newContact);
+  await writeFile(contacts);
+  return newContact;
+}
+
+const updateContact = async (contactId, body) => {
+  const contacts = await readFile();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  contacts[index] = { id: contactId, ...body };
+  await writeFile(contacts);
+  return contacts[index];
+};
 
 module.exports = {
   listContacts,
@@ -16,4 +62,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
