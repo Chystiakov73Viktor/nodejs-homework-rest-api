aa2a408ce8fa51e71250a83e3bd2a6c9e7c39aad
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-01-25 10:54:58 +0200

    prettier

--- middlewares/isValidId.js
+++ middlewares/isValidId.js
@@ -1,13 +1,13 @@
-const {isValidObjectId} = require("mongoose");
+const { isValidObjectId } = require("mongoose");
 const { HttpError } = require("../helpers");
 
 const isValidId = (req, res, next) => {
-    const { contactId } = req.params;
-    if (!isValidObjectId(contactId)) {
-        // next(HttpError(400, `${contactId} is not valid id`))
-        next(HttpError(404, "Not found"))
-    }
-    next();
-}
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    // next(HttpError(400, `${contactId} is not valid id`))
+    next(HttpError(404, "Not found"));
+  }
+  next();
+};
 
 module.exports = isValidId;
